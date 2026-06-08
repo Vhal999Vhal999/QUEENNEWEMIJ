db9a895b5a6dd204d053631383f9f37e8ac8e99a
shuabhkumarmishra412-create <shuabhkumarmishra412@gmail.com>
2026-06-08 22:59:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Bot and owner info
-OWNER_USERNAME = getenv("OWNER_USERNAME", "SHIVANSHDEVS")
-BOT_USERNAME = getenv("BOT_USERNAME", "SapnaMusicRobot")
-BOT_NAME = getenv("BOT_NAME", "SAPNA")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Egoist_Destroyer")
+BOT_USERNAME = getenv("BOT_USERNAME", "@HINATA_MUSIC_PLAYER_BOT")
+BOT_NAME = getenv("BOT_NAME", "HINATA")
 ASSUSERNAME = getenv("ASSUSERNAME", "KHWAAISH_HOON")
 
 # MongoDB
@@ -36,8 +36,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Limits and IDs
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
-LOGGER_ID = int(getenv("LOGGER_ID", -1002018556839))
-OWNER_ID = int(getenv("OWNER_ID", 6762113050))
+LOGGER_ID = int(getenv("LOGGER_ID", -1003939120486))
+OWNER_ID = int(getenv("OWNER_ID", 2145828547))
 
 # Heroku
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
